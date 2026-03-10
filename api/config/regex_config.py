b3a538627da823efd3d44d4a292573dcaeef4309
TeKayy <th3tk.idk@gmail.com>
2026-03-10 23:56:58 +0100

    Added password length limit.

--- api/config/regex_config.py
+++ api/config/regex_config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class RegexConfig:
     universal_name: str = r"^[a-zA-Z][\w\s.-]{2,23}$" # can be used as a universal name validation
     username: str = r"^[a-zA-Z][\w.-]{2,23}$"
-    password: str = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[$&+,:;=?@#|'<>.^*()%!_-]).{10,}$"
+    password: str = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[$&+,:;=?@#|'<>.^*()%!_-]).{10,256}$"
     network_snapshot_name: str = r"^[!-z]{3,24}$"
 
 REGEX_CONFIG = RegexConfig()
